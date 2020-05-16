c84b4162339b61948067c8e0b16a9362a8574df8
pretrehr <raphael.pretre@orange.fr>
2020-05-16 16:09:51 +0200

    test_parsing

--- sportsbetting/basic_test.py
+++ sportsbetting/basic_test.py
@@ -3,8 +3,27 @@
 Fichier de test
 """
 
+import random
+import urllib.request
+
+from bs4 import BeautifulSoup
+
 import sportsbetting
-from sportsbetting.user_functions import *
+from sportsbetting.user_functions import parse_competitions2
+
 
 def test_parsing():
-    assert(1==1)
+    """
+    :return:Test simple
+    """
+    url = "http://www.comparateur-de-cotes.fr/comparateur/football"
+    soup = BeautifulSoup(urllib.request.urlopen(url), features="lxml")
+    sportsbetting.ODDS = {}
+    names = []
+    for line in soup.find_all(attrs={"class": "subhead"}):
+        if "Principaux championnats" in str(line):
+            for link in line.findParent().find_all(["a"]):
+                names.append(link.text.strip())
+    name_competition = random.choice(names)
+    parse_competitions2([name_competition], "football", "betclic", "unibet")
+    assert len(sportsbetting.ODDS) > 0
