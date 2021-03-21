13fe1b03927a988dd4710c6d64449d9821256d0a
pretrehr <43851883+pretrehr@users.noreply.github.com>
2021-03-21 18:36:36 +0100

    verify_ssl=False

--- sportsbetting/__init__.py
+++ sportsbetting/__init__.py
@@ -72,7 +72,7 @@ def find_files(filename, search_path):
             return os.path.abspath(os.path.join(root, filename))
 
 
-ua = UserAgent()
+ua = UserAgent(verify_ssl=False)
 USER_AGENT = sorted(ua.data_browsers["chrome"], key=lambda a: grp(r'Chrome/[^ ]+', a))[-1]
 try:
     PATH_DRIVER = chromedriver_autoinstaller.install(True)
