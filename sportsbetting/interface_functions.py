83e2349dd75611b663260c6886afad8487618be9
pretrehr <raphael.pretre@orange.fr>
2021-03-25 02:07:02 +0100

    hide buttons freebet

--- sportsbetting/interface_functions.py
+++ sportsbetting/interface_functions.py
@@ -257,11 +257,14 @@ def best_match_freebet_interface(window, values):
             window["ODDS_FREEBET"].update(odds_table(what_was_printed), visible=not split_freebet and nb_matches == 1)
             window["RESULT_FREEBET"].update(stakes(what_was_printed), visible=True)
             window["TEXT_FREEBET"].update(visible=True)
-            window["DELETE_MATCH_FREEBET"].update(visible=True)
-            window["RELOAD_ODDS_FREEBET"].update(visible=True)
+            window["DELETE_MATCH_FREEBET"].update(visible=not split_freebet and nb_matches == 1)
+            window["RELOAD_ODDS_FREEBET"].update(visible=not split_freebet and nb_matches == 1)
             for i, elem in enumerate(indicators(what_was_printed)):
                 window["INDICATORS_FREEBET" + str(i)].update(elem[0].capitalize(), visible=True)
                 window["RESULTS_FREEBET" + str(i)].update(elem[1], visible=True)
+            for j in range(i+1, 5):
+                window["INDICATORS_FREEBET" + str(j)].update(visible=False)
+                window["RESULTS_FREEBET" + str(j)].update(visible=False)
         buffer.close()
     except IndexError:
         pass
