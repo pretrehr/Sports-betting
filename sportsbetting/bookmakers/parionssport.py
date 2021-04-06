fa4fb47df3b69bfae9f943ab6d3b03caedc521e4
pretrehr <raphael.pretre@orange.fr>
2021-04-06 23:38:18 +0200

    ignore hidden odds

--- sportsbetting/bookmakers/parionssport.py
+++ sportsbetting/bookmakers/parionssport.py
@@ -202,6 +202,8 @@ def get_sub_markets_players_basketball_parionssport(id_match):
             continue
         if not odd.get("price"):
             continue
+        if "flags" in odd and "hidden" in odd["flags"]:
+            continue
         event = items[market["parent"]]
         player = odd["desc"].split(" - ")[0].split(".")[1]
         limit = odd["desc"].split()[-1].replace(",", ".")
