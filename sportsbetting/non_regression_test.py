0d747ffaab4b6a3027d0f4769d8eb43c16ca70ed
pretrehr <raphael.pretre@orange.fr>
2021-04-30 01:55:48 +0200

    add non regression tests

--- sportsbetting/non_regression_test.py
+++ sportsbetting/non_regression_test.py
@@ -48,3 +48,42 @@ def test_under_condition():
         best_match_under_conditions("parionssport", 1.7, 10, date_min="23/11/2020", date_max="29/11/2020")
     sys.stdout = original_stdout
     assert(are_identical_files(expected, reached))
+
+def test_under_condition_same_site():
+    PATH_DATA_TEST = os.path.dirname(sb.__file__) + "/resources/data_test.json"
+    sb.ODDS = load_odds(PATH_DATA_TEST)
+    original_stdout = sys.stdout
+    reached = os.path.dirname(sb.__file__) + "/tests/result_under_condition_same_site_1_reached.txt"
+    expected = os.path.dirname(sb.__file__) + "/tests/result_under_condition_same_site_1_expected.txt"
+    with open(reached, "w", encoding="utf-8") as f:
+        sys.stdout = f
+        best_match_under_conditions2("pmu", 1.4, 10)
+    sys.stdout = original_stdout
+    assert(are_identical_files(expected, reached))
+    
+    original_stdout = sys.stdout
+    reached = os.path.dirname(sb.__file__) + "/tests/result_under_condition_same_site_2_reached.txt"
+    expected = os.path.dirname(sb.__file__) + "/tests/result_under_condition_same_site_2_expected.txt"
+    with open(reached, "w", encoding="utf-8") as f:
+        sys.stdout = f
+        best_match_under_conditions2("pmu", 2.6, 10)
+    sys.stdout = original_stdout
+    assert(are_identical_files(expected, reached))
+    
+    original_stdout = sys.stdout
+    reached = os.path.dirname(sb.__file__) + "/tests/result_under_condition_same_site_3_reached.txt"
+    expected = os.path.dirname(sb.__file__) + "/tests/result_under_condition_same_site_3_expected.txt"
+    with open(reached, "w", encoding="utf-8") as f:
+        sys.stdout = f
+        best_match_under_conditions2("pmu", 2.6, 10, date_min="24/11/2020")
+    sys.stdout = original_stdout
+    assert(are_identical_files(expected, reached))
+    
+    original_stdout = sys.stdout
+    reached = os.path.dirname(sb.__file__) + "/tests/result_under_condition_same_site_4_reached.txt"
+    expected = os.path.dirname(sb.__file__) + "/tests/result_under_condition_same_site_4_expected.txt"
+    with open(reached, "w", encoding="utf-8") as f:
+        sys.stdout = f
+        best_match_under_conditions2("pmu", 2.6, 10, date_min="24/11/2020", date_max="29/11/2020")
+    sys.stdout = original_stdout
+    assert(are_identical_files(expected, reached))
