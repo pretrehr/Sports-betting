eef4c7936c79242ca07094c15014a67c1a229700
pretrehr <raphael.pretre@orange.fr>
2021-09-06 00:31:07 +0200

    uddate references bool

--- sportsbetting/non_regression_test.py
+++ sportsbetting/non_regression_test.py
@@ -21,69 +21,69 @@ def are_identical_files(filename1, filename2):
     return False
     
 
-def test_under_condition():
+def test_under_condition(update_references=False):
     PATH_DATA_TEST = os.path.dirname(sb.__file__) + "/resources/data_test.json"
     sb.ODDS = load_odds(PATH_DATA_TEST)
     original_stdout = sys.stdout
     reached = os.path.dirname(sb.__file__) + "/tests/result_under_condition_1_reached.txt"
     expected = os.path.dirname(sb.__file__) + "/tests/result_under_condition_1_expected.txt"
-    with open(reached, "w", encoding="utf-8") as f:
+    with open(expected if update_references else reached, "w", encoding="utf-8") as f:
         sys.stdout = f
         best_match_under_conditions("parionssport", 1.7, 10)
     sys.stdout = original_stdout
-    assert(are_identical_files(expected, reached))
+    assert(update_references or are_identical_files(expected, reached))
         
     reached = os.path.dirname(sb.__file__) + "/tests/result_under_condition_2_reached.txt"
     expected = os.path.dirname(sb.__file__) + "/tests/result_under_condition_2_expected.txt"
-    with open(reached, "w", encoding="utf-8") as f:
+    with open(expected if update_references else reached, "w", encoding="utf-8") as f:
         sys.stdout = f
         best_match_under_conditions("parionssport", 1.7, 10, date_min="23/11/2020")
     sys.stdout = original_stdout
-    assert(are_identical_files(expected, reached))
+    assert(update_references or are_identical_files(expected, reached))
     
     reached = os.path.dirname(sb.__file__) + "/tests/result_under_condition_3_reached.txt"
     expected = os.path.dirname(sb.__file__) + "/tests/result_under_condition_3_expected.txt"
-    with open(reached, "w", encoding="utf-8") as f:
+    with open(expected if update_references else reached, "w", encoding="utf-8") as f:
         sys.stdout = f
         best_match_under_conditions("parionssport", 1.7, 10, date_min="23/11/2020", date_max="29/11/2020")
     sys.stdout = original_stdout
-    assert(are_identical_files(expected, reached))
+    assert(update_references or are_identical_files(expected, reached))
 
-def test_under_condition_same_site():
+def test_under_condition_same_site(update_references=False):
     PATH_DATA_TEST = os.path.dirname(sb.__file__) + "/resources/data_test.json"
     sb.ODDS = load_odds(PATH_DATA_TEST)
     original_stdout = sys.stdout
     reached = os.path.dirname(sb.__file__) + "/tests/result_under_condition_same_site_1_reached.txt"
     expected = os.path.dirname(sb.__file__) + "/tests/result_under_condition_same_site_1_expected.txt"
-    with open(reached, "w", encoding="utf-8") as f:
+    with open(expected if update_references else reached, "w", encoding="utf-8") as f:
         sys.stdout = f
         best_match_under_conditions2("pmu", 1.4, 10)
     sys.stdout = original_stdout
-    assert(are_identical_files(expected, reached))
+    assert(update_references or are_identical_files(expected, reached))
     
     original_stdout = sys.stdout
     reached = os.path.dirname(sb.__file__) + "/tests/result_under_condition_same_site_2_reached.txt"
     expected = os.path.dirname(sb.__file__) + "/tests/result_under_condition_same_site_2_expected.txt"
-    with open(reached, "w", encoding="utf-8") as f:
+    with open(expected if update_references else reached, "w", encoding="utf-8") as f:
         sys.stdout = f
         best_match_under_conditions2("pmu", 2.6, 10)
     sys.stdout = original_stdout
-    assert(are_identical_files(expected, reached))
+    assert(update_references or are_identical_files(expected, reached))
     
     original_stdout = sys.stdout
     reached = os.path.dirname(sb.__file__) + "/tests/result_under_condition_same_site_3_reached.txt"
     expected = os.path.dirname(sb.__file__) + "/tests/result_under_condition_same_site_3_expected.txt"
-    with open(reached, "w", encoding="utf-8") as f:
+    with open(expected if update_references else reached, "w", encoding="utf-8") as f:
         sys.stdout = f
         best_match_under_conditions2("pmu", 2.6, 10, date_min="24/11/2020")
     sys.stdout = original_stdout
-    assert(are_identical_files(expected, reached))
+    assert(update_references or are_identical_files(expected, reached))
     
     original_stdout = sys.stdout
     reached = os.path.dirname(sb.__file__) + "/tests/result_under_condition_same_site_4_reached.txt"
     expected = os.path.dirname(sb.__file__) + "/tests/result_under_condition_same_site_4_expected.txt"
-    with open(reached, "w", encoding="utf-8") as f:
+    with open(expected if update_references else reached, "w", encoding="utf-8") as f:
         sys.stdout = f
         best_match_under_conditions2("pmu", 2.6, 10, date_min="24/11/2020", date_max="29/11/2020")
     sys.stdout = original_stdout
-    assert(are_identical_files(expected, reached))
+    assert(update_references or are_identical_files(expected, reached))
